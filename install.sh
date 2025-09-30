fb911f28343b7b7fceecb0e959f78131d91d581f
msh31 <mshendriks03@gmail.com>
2025-09-30 11:07:12 +0200

    fix: install script

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ ln -sf "$(pwd)/.zshrc" ~/.zshrc
 ln -sf "$(pwd)/.tmux.conf" ~/.tmux.conf
 
 mkdir -p ~/.config/ghostty
-mkdir -p ~/.config/nvim
 
 ln -sf "$(pwd)/config/ghostty/config" ~/.config/ghostty/config
-ln -sfn "$(pwd)/config/nvim" ~/.config/nvim
+ln -sf "$(pwd)/config/nvim" ~/.config/nvim
