af2f927052d9d17ff6f2109cc3822f4b81ea7eec
msh31 <mshendriks03@gmail.com>
2025-10-01 15:06:51 +0200

    fix (nvim): set sonokai/atlantis theme

--- config/nvim/lua/michiel/plugins/colorscheme.lua
+++ config/nvim/lua/michiel/plugins/colorscheme.lua
@@ -1,27 +1,12 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-
-	config = function()
-		require("rose-pine").setup({
-      disable_background = true,
-			dark_variant = "moon", -- main, moon, or dawn
-
-			styles = {
-				bold = true,
-				italic = true,
-				transparency = true,
-			},
-
-			enable = {
-				terminal = true,
-				migrations = true, -- Handle deprecated options automatically
-			},
-		})
-
-    vim.cmd("colorscheme rose-pine")
-
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	end
+  {
+    "sainnhe/sonokai",
+    priority = 6969,
+    lazy = false,
+    config = function()
+      vim.g.sonokai_style = "atlantis"
+      vim.g.sonokai_better_performance = 1
+      vim.cmd.colorscheme("sonokai")
+    end,
+  },
 }
