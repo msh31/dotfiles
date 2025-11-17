551b93a406f175f082f80ef47244336c3160a80f
msh31 <180540729+msh31@users.noreply.github.com>
2025-11-17 10:04:16 +0100

    feat: add nomad

---
+++ config/nvim/lua/marco/plugins/nomad.lua
@@ -0,0 +1,11 @@
+return {
+  "nomad/nomad",
+  version = "*",
+  build = function()
+    ---@type nomad.neovim.build
+    local build = require("nomad.neovim.build")
+
+    build.builders.download_prebuilt():build(build.contexts.lazy())
+  end,
+  opts = {},
+}
