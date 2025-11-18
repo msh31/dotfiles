426be310eb2842094013b6df4e7e672f00236846
msh31 <180540729+msh31@users.noreply.github.com>
2025-11-18 12:16:01 +0100

    feat (ghostty): add drunkard shader
    hahahhahahahahahhahahhahahah this is awful

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -sf "$(pwd)/.tmux.conf" ~/.tmux.conf
 ln -sf "$(pwd)/.aerospace.toml" ~/.aerospace.toml
 
 mkdir -p ~/.config/ghostty
-
 ln -sf "$(pwd)/config/ghostty/config" ~/.config/ghostty/config
+ln -sf "$(pwd)/config/ghostty/shaders" ~/.config/ghostty/shaders
+
 ln -sf "$(pwd)/config/nvim" ~/.config/nvim
