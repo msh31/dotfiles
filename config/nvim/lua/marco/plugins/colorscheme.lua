54ee73f54a03def61889ffa5bd702e2b9da350cd
msh31 <180540729+msh31@users.noreply.github.com>
2025-11-19 09:46:48 +0100

    change: colorschemes

--- config/nvim/lua/marco/plugins/colorscheme.lua
+++ config/nvim/lua/marco/plugins/colorscheme.lua
@@ -6,7 +6,11 @@ return {
     config = function()
       vim.g.sonokai_style = "atlantis"
       vim.g.sonokai_better_performance = 1
-      vim.g.sonokai_transparent_background = 2
+      vim.g.sonokai_transparent_background = 0
+
+      vim.g.sonokai_colors_override = {
+        bg0 = "#282828",
+      }
       vim.cmd.colorscheme("sonokai")
     end,
   },
