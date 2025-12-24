f081ed6d98c8506236216f328fedee06bab77258
Marco <marco@marco007.dev>
2025-12-24 22:04:31 +0100

    update wallpapers

--- config/scripts/set-time-based-wallpaper.sh
+++ config/scripts/set-time-based-wallpaper.sh
@@ -3,12 +3,13 @@
 HOUR=$(date +%H)
 
 if [ "$HOUR" -ge 0 ] && [ "$HOUR" -lt 6 ]; then
-    NIGHT_PAPER="$HOME/Pictures/the-folder/wallpapers/two.jpg"
+    NIGHT_PAPER="$HOME/Pictures/the-folder/wallpapers/3.jpg"
+    NIGHT_PAPER_V="$HOME/Pictures/the-folder/wallpapers/vertical.jpeg"
     hyprctl hyprpaper wallpaper "DP-1,$NIGHT_PAPER"
     hyprctl hyprpaper wallpaper "DP-3,$NIGHT_PAPER"
-    hyprctl hyprpaper wallpaper "DP-2,$NIGHT_PAPER"
+    hyprctl hyprpaper wallpaper "DP-2,$NIGHT_PAPER_V"
 else
-    DAY_PAPER="$HOME/Pictures/wallpaper2.jpg"
+    DAY_PAPER="$HOME/Pictures/wallpapers/wallpaper2.jpg"
     hyprctl hyprpaper wallpaper "DP-1,$DAY_PAPER"
     hyprctl hyprpaper wallpaper "DP-3,$DAY_PAPER"
     hyprctl hyprpaper wallpaper "DP-2,$DAY_PAPER"
