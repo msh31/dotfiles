cb5f1854608888dd06a5c454662407bef7d2c753
Marco <marco@marco007.dev>
2026-02-01 00:50:38 +0100

    update install script
    removes hyprland defaults
    links quickshell

--- install.sh
+++ install.sh
@@ -9,6 +9,8 @@ ln -sf "$(pwd)/config/ghostty/shaders" ~/.config/ghostty/shaders
 ln -sf "$(pwd)/config/nvim" ~/.config/nvim
 
 ln -sf "$(pwd)/config/hyprpanel" ~/.config/hyprpanel
+
+rm -rf ~/.config/hypr
 ln -sf "$(pwd)/config/hypr" ~/.config/hypr
 
 ln -sf "$(pwd)/config/wofi" ~/.config/wofi
@@ -30,3 +32,4 @@ sudo mkdir -p /etc/keyd
 sudo ln -sf "$(pwd)/service-configs/keyd/semico.conf" /etc/keyd/semico.conf
 
 sudo ln -sf "$(pwd)/config/gamemode" ~/.config/gamemode
+ln -sf "$(pwd)/config/quickshell" ~/.config/quickshell
