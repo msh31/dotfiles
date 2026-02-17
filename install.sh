91e865a03ca56121d7d20bd6339c0fdac1246760
marco <marco@marco007.dev>
2026-02-17 11:32:33 +0100

    add kitty config (default is still ghostty)

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ ln -sf "$(pwd)/.tmux.conf" ~/.tmux.conf
 ln -sf "$(pwd)/.aerospace.toml" ~/.aerospace.toml
 
 mkdir -p ~/.config/ghostty
-ln -sf "$(pwd)/config/ghostty/config" ~/.config/ghostty/config
-ln -sf "$(pwd)/config/ghostty/shaders" ~/.config/ghostty/shaders
+ln -sf "$(pwd)/config/ghostty" ~/.config/ghostty
 
 ln -sf "$(pwd)/config/nvim" ~/.config/nvim
+# ln -sf "$(pwd)/config/kitty" ~/.config/kitty
+
